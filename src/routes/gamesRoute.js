794544150dbb2a82b7c47c04d63e553ba4e5996f
Ranbut <Ranbut_Beater@protonmail.com>
2023-02-06 23:48:06 -0300

    feat: Added schemas and middleware validation

--- src/routes/gamesRoute.js
+++ src/routes/gamesRoute.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { GetGames } from '../controllers/games/getGamesController.js';
 import { PostGames } from '../controllers/games/postGamesController.js';
+import { validateSchema } from '../middlewares/validateSchema.js';
+import { gamesSchema } from '../schemas/gamesSchema.js';
 
 const router = Router();
 
 router.get('/games', GetGames);
-router.post('/games', PostGames);
+router.post('/games', validateSchema(gamesSchema), PostGames);
 
 export default router;
--- src/routes/rentalsRoute.js
+++ src/routes/rentalsRoute.js
@@ -3,11 +3,13 @@ import { GetRentals } from '../controllers/rentals/getRentalsController.js';
 import { PostRentals } from '../controllers/rentals/postRentalsController.js';
 import { PostRentalsIDReturn } from '../controllers/rentals/postRentalsIDReturnController.js';
 import { DeleteRentalsID } from '../controllers/rentals/deleteRentalsIDController.js';
+import { validateSchema } from '../middlewares/validateSchema.js';
+import { rentalsSchema } from '../schemas/rentalsSchema.js';
 
 const router = Router();
 
 router.get('/rentals', GetRentals);
-router.post('/rentals', PostRentals);
+router.post('/rentals', validateSchema(rentalsSchema), PostRentals);
 router.post('/rentals/:id/return', PostRentalsIDReturn);
 router.delete('/rentals/:id/return', DeleteRentalsID);
 
