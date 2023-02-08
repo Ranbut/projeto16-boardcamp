99c4beff731957abfcf96c36fd57e42d7fded796
Ranbut <Ranbut_Beater@protonmail.com>
2023-02-07 23:47:49 -0300

    feat: post status of /games

--- src/controllers/games/postGamesController.js
+++ src/controllers/games/postGamesController.js
@@ -2,16 +2,27 @@ import { db } from "../../database/database.connection.js";
 
 export async function PostGames (req, res) {
 
-    const { name, image, stockTotal, pricePerDay} = req.body;
+    const { name, image, stockTotal, pricePerDay } = req.body;
 
     try{
-        console.log(name);
 
-        const games = await db.query(`INSERT INTO games (name ,image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)`, [name, image, stockTotal, pricePerDay]);
+        if(name && stockTotal > 0 && pricePerDay > 0){
 
-        const results = games.rows;
+            const games = await db.query(`SELECT name FROM games WHERE name = $1`, [name]);
 
-        res.status(201).send(results);
+            const results = games.rows[0];
+    
+            if(results) return res.status(409).send("Tábuleiro já existe.");
+
+            console.log(results);
+
+            db.query(`INSERT INTO games (name , image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)`, [name, image, stockTotal, pricePerDay]);
+
+            res.status(201).send("Tábuleiro adicionado com sucesso!");
+        }
+        else
+            return res.status(400).send("Dados do HTTP body estão inválidos.");
+
     }
     
     catch(err){
