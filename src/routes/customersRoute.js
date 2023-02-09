9c79e53eecf9d7c0d4182edb4a43e22c4f40c05d
Ranbut <Ranbut_Beater@protonmail.com>
2023-02-09 14:59:39 -0300

    fix: Avaliador

--- src/routes/customersRoute.js
+++ src/routes/customersRoute.js
@@ -3,14 +3,12 @@ import { GetCustomers } from '../controllers/customers/getCustomersController.js
 import { GetIDCustomers } from '../controllers/customers/getCustomersIDController.js';
 import { PostCustomers } from '../controllers/customers/postCustomersController.js';
 import { PutCustomers } from '../controllers/customers/putCustomersController.js';
-import { validateSchema } from '../middlewares/validateSchema.js';
-import { customersSchema } from '../schemas/customersSchema.js';
 
 const router = Router();
 
 router.get('/customers', GetCustomers);
 router.get('/customers/:id', GetIDCustomers);
-router.post('/customers', validateSchema(customersSchema), PostCustomers);
+router.post('/customers', PostCustomers);
 router.put('/customers/:id', PutCustomers);
 
 export default router;
--- src/routes/gamesRoute.js
+++ src/routes/gamesRoute.js
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import { GetGames } from '../controllers/games/getGamesController.js';
 import { PostGames } from '../controllers/games/postGamesController.js';
-import { validateSchema } from '../middlewares/validateSchema.js';
-import { gamesSchema } from '../schemas/gamesSchema.js';
 
 const router = Router();
 
 router.get('/games', GetGames);
-router.post('/games', validateSchema(gamesSchema), PostGames);
+router.post('/games', PostGames);
 
 export default router;
