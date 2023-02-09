66fad196dcea043bc2ed11bcdca0b8bf425827fa
Ranbut <Ranbut_Beater@protonmail.com>
2023-02-09 16:22:12 -0300

    fix: Avaliador

--- src/schemas/customersSchema.js
+++ src/schemas/customersSchema.js
@@ -4,5 +4,5 @@ export const customersSchema = joi.object({
     name: joi.string().allow(null, ''),
     phone: joi.string().allow(null, ''),
     cpf: joi.string().allow(null, ''),
-    birthday: joi.string.allow(null, '')
+    birthday: joi.string().allow(null, '')
   });
