5864cd0c31ac6448fc1c4f5171df87dc6c6ae772
Ranbut <Ranbut_Beater@protonmail.com>
2023-02-09 18:57:52 -0300

    fix: Avaliador

--- src/controllers/rentals/postRentalsController.js
+++ src/controllers/rentals/postRentalsController.js
@@ -12,19 +12,19 @@ export async function PostRentals (req, res) {
         const customer = await db.query(`SELECT id FROM customers WHERE id = ${customerId}`);
         const resultCustomer = customer.rows[0];
 
-        const games = await db.query(`SELECT * FROM games WHERE id=${gameId}`);
-        const resultGame = games.rows[0];
+        const game = await db.query(`SELECT * FROM games WHERE id=${gameId}`);
+        const resultGame = game.rows[0];
 
-        if(!resultCustomer || !resultGame || daysRented < 1 || games.length >= resultGame.stockTotal) return res.sendStatus(400);
+        const usedGames = await db.query(`SELECT * FROM rentals WHERE "gameId"=${gameId}`);
+        const resultsUsedGames = usedGames.rows;
 
+        if(!resultCustomer || !resultGame || daysRented < 1 || resultsUsedGames.length >= resultGame.stockTotal) return res.sendStatus(400);
 
         const dateFormat = 'YYYY-MM-DD'    
         const today = new Date();
         const rentDate = dayjs(today).format(dateFormat);
 
         const originalPrice = resultGame.pricePerDay * daysRented;
-
-        console.log(customerId, gameId, rentDate, daysRented, originalPrice);
 
         await db.query(`INSERT INTO rentals ("customerId" , "gameId", "rentDate", "daysRented", "returnDate", "originalPrice", "delayFee") 
         VALUES (${customerId}, ${gameId}, '${rentDate}', ${daysRented}, null, ${originalPrice}, null);`);
