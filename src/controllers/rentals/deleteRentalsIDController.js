c39aeaefa4ea91a595e6d83b375591bde1b6df5b
Ranbut <Ranbut_Beater@protonmail.com>
2023-02-09 21:45:29 -0300

    fix: Avaliador

--- src/controllers/rentals/deleteRentalsIDController.js
+++ src/controllers/rentals/deleteRentalsIDController.js
@@ -12,6 +12,8 @@ export async function DeleteRentalsID (req, res) {
 
         if(!results) return res.sendStatus(404);
 
+        if(results.returnDate !== null) return res.sendStatus(400);
+
         await db.query(`DELETE FROM rentals WHERE id=${id}`);
 
         res.sendStatus(200);
