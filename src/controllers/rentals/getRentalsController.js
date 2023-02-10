318458ecc529862e19d342fd04c9938d5d00f5ed
Ranbut <Ranbut_Beater@protonmail.com>
2023-02-09 22:10:33 -0300

    fix: Avaliador

--- src/controllers/rentals/getRentalsController.js
+++ src/controllers/rentals/getRentalsController.js
@@ -4,8 +4,20 @@ export async function GetRentals (req, res) {
 
     try{
         const rentals = await db.query("SELECT * FROM rentals");
+        const customers = await db.query(`SELECT * FROM customers`);
+        const games = await db.query(`SELECT * FROM games`);
 
-        const results = rentals.rows;
+        const results = rentals.rows.map(allocation => ({
+            ...allocation,
+            customer: {
+                id: customers.rows.find(customer => customer.id === allocation.customerId).id,
+                name: customers.rows.find(customer => customer.id === allocation.customerId).name,
+            },
+            game: {
+                id: games.rows.find(game => game.id === allocation.customerId).id,
+                name: games.rows.find(game => game.id === allocation.customerId).name,
+            }
+          }))
 
         res.status(200).send(results);
     }
