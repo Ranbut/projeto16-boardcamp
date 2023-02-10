9aac309a05fbc06a224412be84bf657db0a75c0f
Ranbut <Ranbut_Beater@protonmail.com>
2023-02-10 18:49:46 -0300

    fix: Avaliador

--- src/controllers/rentals/getRentalsController.js
+++ src/controllers/rentals/getRentalsController.js
@@ -1,7 +1,7 @@
 import { db } from "../../database/database.connection.js";
 
 export async function GetRentals (req, res) {
-        const {customerId, gameId,status, startDate } = req.query;
+        const {customerId, gameId,status, startDate, order, desc  } = req.query;
     try{
 
         const dateRegex = /^\d{4}-\d{2}-\d{2}$/;
